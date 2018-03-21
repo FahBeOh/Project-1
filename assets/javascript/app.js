cee9cd4c63c0cdb1d580963ba488d41bb178ae52
Maverick <embrymaverick@gmail.com>
2018-03-20 20:26:22 -0700

    Append definitions & examples

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,56 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-    //Here is the user input word
-    
    
-   
-    $(".urban").on("click", function(process){
 
+
+
+    $(".urban").on("click", function (process) {
+
+        $(".urbanDef").empty();
+
+        //Disables the page to refresh itself
         process.preventDefault();
 
+        //Targeting user input
         term = $("#user-input").val().trim();
 
         console.log(term);
 
-       
 
-    //Contact the API
-    $.get(
-        "http://api.urbandictionary.com/v0/define?term=" + term
-      ).then(function (response) {
-        
-  
-        console.log(response);
 
-        
+
+
+        //Contact the API
+        $.get(
+            "http://api.urbandictionary.com/v0/define?term=" + term
+        ).then(function (response) {
+
+
+            console.log(response);
+            console.log(response.list[0].definition);
+
+            $(".userInput").html("Word searched: " + term);
             
-           
+            $(".urbanDef").html("Definition: " + response.list[0].definition);
+          
+            $(".urbanEx").html("Example: " + response.list[0].example);
 
-          })
-          $("#user-input").val("");
+        })
+
+        //Erase text after user hits submit
+        $("#user-input").val("");
+
        
-      });
+
+       
+
+    });
+
+
     
 
-     
+
 
 
 
