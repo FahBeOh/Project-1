c31fdc4d3039605e7835710aebf69c0d36f569e1
Fabio Restrepo <fabio@Fabios-MacBook-Pro.local>
2018-03-23 11:01:16 -0700

    Updated with responsive background and functioning giphy api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
-    console.log("ready!");
+    console.log("ready!");  
 
-   
-
-
+// Function for "get" giphy Api and displying it to page
+    function giphy(){
+        var apiKey = "eAFKeOjlTldUvzjggovAVPjRKowwoyai";
+        var queryURL = "https://api.giphy.com/v1/gifs/random?rating=g&tag=" + term + "&api_key=" + apiKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response){
+            console.log(response);
+            var displayGif = $("<img class='img-fluid mr-3 mb-3'>");
+            $(".gifHolder").html(displayGif);
+            displayGif.attr("src", response.data.images.fixed_width.url); 
+        })
+    }
 
     $(".urban").on("click", function (process) {
 
@@ -17,33 +28,25 @@ $(document).ready(function () {
 
         console.log(term);
 
-
-
-
-
         //Contact the API
         $.get(
-            "http://api.urbandictionary.com/v0/define?term=" + term
+            "https://api.urbandictionary.com/v0/define?term=" + term
         ).then(function (response) {
-
 
             console.log(response);
             console.log(response.list[0].definition);
 
-            $(".userInput").html("Word searched: " + term);
+            $(".userInput").html("<b>Word searched</b>: " + term);
             
-            $(".urbanDef").html("Definition: " + response.list[0].definition);
+            $(".urbanDef").html("<b>Definition</b>: " + response.list[0].definition);
           
-            $(".urbanEx").html("Example: " + response.list[0].example);
-
+            $(".urbanEx").html("<b>Example</b>: " + response.list[0].example);
         })
+        // Calling giphy api function
+        giphy();
 
         //Erase text after user hits submit
         $("#user-input").val("");
-
-       
-
-       
 
     });
 
