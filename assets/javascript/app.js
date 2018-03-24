1676d4b0ce526ef2652f7c86927eca57f782ad0c
Maverick <embrymaverick@gmail.com>
2018-03-24 11:34:44 -0700

    news API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    console.log("ready!");  
+    console.log("ready!");
 
-// Function for "get" giphy Api and displying it to page
-    function giphy(){
+    // Function for "get" giphy Api and displying it to page
+    function giphy() {
         var apiKey = "eAFKeOjlTldUvzjggovAVPjRKowwoyai";
         var queryURL = "https://api.giphy.com/v1/gifs/random?rating=g&tag=" + term + "&api_key=" + apiKey;
         $.ajax({
             url: queryURL,
             method: "GET",
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response);
             var displayGif = $("<img class='img-fluid mr-3 mb-3'>");
             $(".gifHolder").html(displayGif);
-            displayGif.attr("src", response.data.images.fixed_width.url); 
+            displayGif.attr("src", response.data.images.fixed_width.url);
         })
     }
 
@@ -28,6 +28,22 @@ $(document).ready(function () {
 
         console.log(term);
 
+        var url = 'https://newsapi.org/v2/top-headlines?' +
+            'q=' + term + '&' +
+            'from=2018-03-24&' +
+            'sortBy=popularity&' +
+            'apiKey=ed60c9f213c14312a011b71f6ecc7d29';
+
+        var req = new Request(url);
+
+        fetch(req)
+            .then(function (response) {
+                console.log(response.json());
+                console.log("articles: "+response.json().articles);
+            })
+
+            // $(".articles").html()
+
         //Contact the API
         $.get(
             "https://api.urbandictionary.com/v0/define?term=" + term
@@ -37,9 +53,9 @@ $(document).ready(function () {
             console.log(response.list[0].definition);
 
             $(".userInput").html("<b>Word searched</b>: " + term);
-            
+
             $(".urbanDef").html("<b>Definition</b>: " + response.list[0].definition);
-          
+
             $(".urbanEx").html("<b>Example</b>: " + response.list[0].example);
         })
         // Calling giphy api function
@@ -51,7 +67,7 @@ $(document).ready(function () {
     });
 
 
-    
+
 
 
 
